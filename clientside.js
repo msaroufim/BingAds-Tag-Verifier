200cfc76d02e6b688212c756555523a8513b644f
Jamie Chung <me@jamiechung.me>
2015-02-10 16:14:10 -0800

    Checking that uetq is defined
    This way you do not pepper the error console

--- clientside.js
+++ clientside.js
@@ -1,10 +1,10 @@
 //todo communicate back and forth between clientside.js and popup.js
 //
 
-if (uetq.domain === "bat.bing.com") {
-	alert('Boom! You are now using UET to track your conversions');
-}
-
-else {
-	alert("Bummer... UET script was not correctly loaded")
+if (typeof uetq != 'undefined') {
+    if (uetq.domain === 'bat.bing.com') {
+        alert('Boom! You are now using UET to track your conversions');
+    } else {
+        alert("Bummer... UET script was not correctly loaded")
+    }
 }
